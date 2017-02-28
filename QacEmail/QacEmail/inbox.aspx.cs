1b7302ef0ae93e264f2b0edf2672e925c6815e5e
josephgordon14 <josephgordon14@gmail.com>
2017-02-28 16:50:57 +0000

    some inbox things. unfinished

--- QacEmail/QacEmail/inbox.aspx.cs
+++ QacEmail/QacEmail/inbox.aspx.cs
@@ -4,15 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace QacEmail
 {
     public partial class inbox : System.Web.UI.Page
     {
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataReader rdr;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["login"] == null)
                 Response.Redirect("login.aspx");
+
+            con = new SqlConnection(Session["conString"].ToString());
+            cmd = new SqlCommand();
+
+            con.Open();
+            cmd.Connection = con;
+
+            inbox_load();
+        }
+
+        public void inbox_load()
+        {
+
         }
     }
 }
