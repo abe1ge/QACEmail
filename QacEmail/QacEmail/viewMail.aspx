908f08a9b6fa26a7d1a8cdd491f51498c1bd775b
abe1ge <abe1ge@github.com>
2017-03-03 15:27:25 +0000

    view mail working

--- QacEmail/QacEmail/viewMail.aspx
+++ QacEmail/QacEmail/viewMail.aspx
@@ -1,3 +1,28 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/pow.Master" AutoEventWireup="true" CodeBehind="viewMail.aspx.cs" Inherits="QacEmail.viewMail" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="form-horizontal">
+        <fieldset>
+            <!-- Form Name -->
+            <legend>Form Name</legend>
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="fromText">From :</label>
+                <div class="col-md-4">
+                    <asp:TextBox ID="FromText" runat="server" name="fromText" type="text" class="form-control input-md" disabled="disabled"></asp:TextBox>
+                </div>
+            </div>
+
+            <!-- Textarea -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="MailArea">Message</label>
+                <div class="col-md-4">
+                    <asp:TextBox ID="message" runat="server" class="form-control" name="MailArea" disabled="disabled"></asp:TextBox>
+                </div>
+            </div>
+
+        </fieldset>
+    </div>
+
 </asp:Content>
--- QacEmail/QacEmail/viewMail.aspx.cs
+++ QacEmail/QacEmail/viewMail.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,35 @@ namespace QacEmail
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (Session["login"] == null)
+                Response.Redirect("login.aspx");
+
+            string id = Request["id"];
+
+            SqlCommand cmd;
+            SqlConnection con;
+
+            con = new SqlConnection(Session["conString"].ToString());
+            cmd = new SqlCommand();
+
+            con.Open();
+            cmd.Connection = con;
+
+            //Select mailFrom,txt from emails where email = 12
+            cmd.CommandText = "Select mailFrom,txt from emails where email =" + id;
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                if (rdr.Read())
+                {
+                    FromText.Text = rdr["mailFrom"].ToString();
+                    message.Text = rdr["txt"].ToString();
+                }
+                else
+                {
+                    Response.Redirect("inbox.aspx");
+                }
+            }
         }
     }
 }
